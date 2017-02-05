a432eb64430d1a0347795ee34eb22507037e78fa
srnesdoly <srnesdoly@me.com>
2017-02-05 04:31:52 -0500

    addition of journal entries page

---
+++ jentries.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <meta name="author" content="VMS&amp;SN&amp;BB&amp;VH">
+
+  <link rel="shortcut icon" href="img/favicon.png">
+
+  <title>QThrive - Journal </title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/main.css" rel="stylesheet">
+
+  <!-- Fonts from Google Fonts -->
+  <link href='http://fonts.googleapis.com/css?family=Lato:300,400,900' rel='stylesheet' type='text/css'>
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+
+  <style>
+    #headerwrap {
+        padding-top: 100px;
+        min-height: 850px;
+        background-image: url("img/sail.jpg");
+        background-size: cover;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+    }
+  </style>
+
+</head>
+<body>
+<!-- Fixed navbar -->
+<div class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+      <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php"> <img src="img/QThrive.png" height="50"> <b>QThrive</b> </a>
+      </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-right">
+            <li><a>Welcome, Vinyas.</a></li>
+        </ul>
+      </div>
+  </div>
+</div>
+
+<div id="headerwrap">
+<div class="container">
+    <h1>Journal Entries</h1>
+
+    <!-- List of Diary Entries -->
+    <div class="container">
+        <ul class="list-group j">
+            <li class="list-group-item">
+                <h4 class="list-group-item-heading">January 29th, 2017</h4>
+                <p class="list-group-item-text">I had an absolute blast today at the outdoor rink. Met a new friend from Finland with the greatest accent! Things are definitely looking up. It will be my turn to cook for my housemates tomorrow, which I am beyond excited for. Key ingredients are fresh basil and chicken. I hope they like it.</p>
+            </li>
+            <li class="list-group-item">
+                <h4 class="list-group-item-heading">February 2nd, 2017</h4>
+                <p class="list-group-item-text">Turns out that Thursday's are not my favourite day of the week. I found it exceedingly difficult to get out of bed this morning. My 4th assignment this week is due in a couple of hours here. Will journal more tomorrow.</p>
+            </li>
+            <li class="list-group-item">
+                <h4 class="list-group-item-heading">February 3rd, 2017</h4>
+                <p class="list-group-item-text">I am not sure I should be awake at this hour. 04:30 is pushing it, even for me. I just can't seem to sleep.</p>
+            </li>
+            <li class="list-group-item">
+                <h4 class="list-group-item-heading">February 5th, 2017</h4>
+                <p class="list-group-item-text">I just got accepted to my dream master's program! Great day!</p>
+            </li>
+        </ul>
+    </div>
+</div>
+</div>
+
+<!-- footer section -->
+<div class="container">
+
+  <p class="centered" style="margin-top: 40px;">Created by Ben, Sean, Vinyas, &amp; Vinith</p>
+
+</div>
+
+</body>
+</html>
