af226879fed3dc5a38ee6aebf638e0083cc84dd8
vinyasHarish95 <vinyas.harish95@gmail.com>
2017-02-05 04:29:14 -0500

    Sailboat + modal!

--- journal.php
+++ journal.php
@@ -75,7 +75,45 @@ if (isset($_POST['submit'])) {
 		#bottomCredits {
 			color: white;
 		}
-		</style>
+			/* The Modal (background) */
+		#modal {
+			display: none; /* Hidden by default */
+			position: fixed; /* Stay in place */
+			z-index: 1; /* Sit on top */
+			padding-top: 100px; /* Location of the box */
+			left: 0;
+			top: 0;
+			width: 100%; /* Full width */
+			height: 100%; /* Full height */
+			overflow: auto; /* Enable scroll if needed */
+			background-color: rgb(0,0,0); /* Fallback color */
+			background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+		}
+
+		/* Modal Content */
+		#modal-content {
+			background-color: #fefefe;
+			margin: auto;
+			padding: 20px;
+			border: 1px solid #888;
+			width: 80%;
+		}
+
+		* The Close Button */
+		#close {
+			color: #aaaaaa;
+			float: right;
+			font-size: 28px;
+			font-weight: bold;
+		}
+
+		#close:hover,
+		#close:focus {
+			color: #000;
+			text-decoration: none;
+			cursor: pointer;
+		}
+</style>
 
 </head>
 <body>
@@ -113,8 +151,19 @@ if (isset($_POST['submit'])) {
   </div>
 </div>
 
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>Sentiment analysis...</p>
+  </div>
+
+</div>
+
 <script>
-		// single example
+		//Indico API call
 		$('#sentiment').click(function(event){
 				event.preventDefault();
 				var form= $('form').serializeArray();
@@ -137,6 +186,32 @@ if (isset($_POST['submit'])) {
 						document.cookie = 'surprise='+emotionValues.results.surprise+'; expires=' + now.toUTCString() + '; path=/';
 				});
 		})
+		// Get the modal
+		var modal = document.getElementById('myModal');
+
+		// Get the button that opens the modal
+		var btn = document.getElementById("sentiment");
+
+		// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName("close")[0];
+
+		// When the user clicks on the button, open the modal
+		btn.onclick = function() {
+			modal.style.display = "block";
+		}
+
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() {
+		modal.style.display = "none";
+		}
+
+		//When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+			if (event.target == modal) {
+				modal.style.display = "none";
+			}
+}
+
 </script>
 </body>
 </html>
